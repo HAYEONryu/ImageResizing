0e2575ef3dee5bbf0655c3bb463fea51699ec535
HAYEONryu <21700470@handong.edu>
2021-07-20 02:26:04 +0900

    원본파일 삭제 기능 추가 (주석)

--- src/main/java/com/example/demo/FileUploadController.java
+++ src/main/java/com/example/demo/FileUploadController.java
@@ -115,7 +115,10 @@ public class FileUploadController {
 	                System.out.println("fileSize 확인 "+fileSize);
 	                FileDto resizedDto = new FileDto(Long.toString(fileSize)+ " kb",w+"/"+h,"resized_"+fileName, file.getContentType());
 		            list.add(resizedDto);
-	     
+		            
+		            //아래 주석을 풀면 원본 파일이 삭제 된다. 
+		            //deleteFile(originalFile);
+		            //deleteFile(newFile);
 	            }catch (Exception e){
 	     
 	                e.printStackTrace();
@@ -128,6 +131,20 @@ public class FileUploadController {
 	    model.addAttribute("files", list);
 	    return "result";
 	}
+	public void deleteFile(File deleteFile) {
+            
+        // 파일이 존재하는지 체크 존재할경우 true, 존재하지않을경우 false
+        if(deleteFile.exists()) {
+            
+            // 파일을 삭제합니다.
+            deleteFile.delete(); 
+            
+            System.out.println("파일을 삭제하였습니다.");
+            
+        } else {
+            System.out.println("파일이 존재하지 않습니다.");
+        }
+    }
 }
 
 
